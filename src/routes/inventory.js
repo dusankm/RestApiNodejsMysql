9b3ce70dea19818ec8b01bf907828e523813b184
dusankm <dusankm95@gmail.com>
2020-11-28 15:36:10 +0100

    add path /add-inventory and /inventory

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -1,11 +1,30 @@
+const { query } = require('express');
 const express =require('express');
 const router = express.Router();
 const mysqlConnection = require('../database');
 
-router.get('/', (req,res) =>{
+router.get('/inventory', (req,res) =>{
     mysqlConnection.query('SELECT * FROM inventory', (err, rows, fields) => {
         if(!err){
             res.json(rows);
+        }else{
+            console.log('Error', err)
+        }
+    });
+});
+
+router.get('/add-inventory', (req,res) =>{
+    const{id, name, stock}= req.body;
+    const query=`
+        SET @id =?;
+        SET @name =?;
+        SET @stock =?;
+
+        CALL inventoryAddEdit(@id, @name, @stock);
+    `;
+    mysqlConnection.query(query,[id,name,salary],(err, rows, fields) => {
+        if(!err){
+            res.json({Stutus:'OK', RSP:'Inventario Guardado'});
         }else{
             console.log('Error', err)
         }
